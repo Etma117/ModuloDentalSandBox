1a113f5d45232b3d6260aa27ffdf91521576b1a8
ivansoza <ivansz123@gmail.com>
2023-09-07 14:38:29 -0500

    cambios en el admin

--- core/core/settings.py
+++ core/core/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'pacientes',
     'usuarios',
     'catalogos',
+    'clinicas',
+    
     
 ]
 
--- core/usuarios/models.py
+++ core/usuarios/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from clinicas.models import Clinica
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    TIPOS_USUARIO = (
+        ('administrador', 'Administrador'),
+        ('medico', 'Médico'),
+        ('asistente', 'Asistente'),
+        ('paciente', 'Paciente'),
+    )
+
+    tipo_usuario = models.CharField(choices=TIPOS_USUARIO, max_length=15)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+    numero = models.CharField(max_length=15, null=True, blank=True)
+    clinicas = models.ManyToManyField(Clinica)
+
+    def __str__(self):
+        return self.username
