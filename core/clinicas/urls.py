e349ac674d0e0599dea842494ac7a297305ea216
Dany248118 <ouen_daniel@hotmail.com>
2024-01-26 15:06:56 -0600

    CRD clinicas

--- core/clinicas/urls.py
+++ core/clinicas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import clinicaCrear, Clinicas, vistaClinica, eliminarClinica
 
 urlpatterns = [
-    path('verClinicas/', views.clinicas, name='clinicas'),
-    path('clinica0/', views.vistaClinica, name='vistaClinicas'),
-    path('crearClinica/', views.crearClinica, name='nuevaClinica'),
+    path('verClinicas/', Clinicas.as_view(), name='clinicas'),
+    path('clinica/<int:pk>', vistaClinica.as_view(), name='vistaClinicas'),
+    path('crearClinica/', clinicaCrear.as_view(), name='nuevaClinica'),
+    path('eliminarClinica/<int:pk>', eliminarClinica.as_view(), name='eliminarClinica'),
 ]
