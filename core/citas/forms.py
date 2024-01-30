47354e507b09a4f7d74861899bd6e4d9bfd2693c
Etma117 <polllfuentes@gmail.com>
2024-01-30 14:55:53 -0600

    HorariosCita - TemplatesCitas

--- core/citas/forms.py
+++ core/citas/forms.py
@@ -1,32 +1,31 @@
 from django import forms
-from .models import Cita
+from .models import Cita, HoraTrabajo
 from usuarios.models import CustomUser
 from clinicas.models import Clinica
 from django.contrib.auth.models import Group
 
 from datetime import datetime, timedelta
 
+
 class CitaForm(forms.ModelForm):
     class Meta:
         model = Cita
-        fields = ['paciente', 'clinica', 'doctor', 'fecha', 'hora_inicio', 'hora_termino', 'estado_cita'] 
+        fields = ['paciente', 'clinica', 'dentista', 'fecha', 'hora_trabajo','estado_cita'] 
         labels = {
             'paciente' : "Paciente",
-            'doctor': "Doctor",
+            'dentista': "Dentista",
             'fecha' : "Fecha",  
             'clinica' : 'Clinicas',         
-            'hora_inicio' : "Hora de inicio",
-            'hora_termino' : "Hora de termino",
+            'hora_trabajo' : "Seleciona un Horario Disponible",
             'estado_cita' : "Estado de la cita"
 
         }
         widgets = {
             'paciente': forms.Select(attrs={'class': 'form-control'}),
-            'doctor': forms.Select(attrs={'class': 'form-control'}),
+            'dentista': forms.Select(attrs={'class': 'form-control'}),
             'clinica' : forms.Select(attrs={'class': 'form-control'}), 
-            'fecha': forms.DateInput(attrs={'class': 'form-control', 'type':'date'}),
-            'hora_inicio' : forms.TimeInput(attrs={'class': 'form-control', 'type':'time'}),            
-            'hora_termino' : forms.TimeInput(attrs={'class': 'form-control', 'type':'time'}),
+            'fecha': forms.DateInput(attrs={'class': 'form-control', 'type':'date'}, format='%Y-%m-%d'),
+            'hora_trabajo' : forms.Select(attrs={'class': 'form-control'}),
             'estado_cita': forms.Select(attrs={'class': 'form-control'})
             
         }
@@ -39,34 +38,39 @@ class CitaForm(forms.ModelForm):
         self.fields['paciente'].queryset = CustomUser.objects.filter(groups=pacientes_group)
 
         # Filtrar opciones del campo doctor según el grupo "doctores"
-        doctores_group = Group.objects.get(name='Doctores')
-        self.fields['doctor'].queryset = CustomUser.objects.filter(groups=doctores_group)
+        dentista_group = Group.objects.get(name='Dentista')
+        self.fields['dentista'].queryset = CustomUser.objects.filter(groups=dentista_group)
 
         # Calcula la fecha de mañana y establece como valor predeterminado
         tomorrow = (datetime.now() + timedelta(days=1)).date()
         self.initial['fecha'] = tomorrow
-    
+
+
+        # Filtrar los horarios de trabajo que no están ocupados
+        self.fields['hora_trabajo'].queryset = HoraTrabajo.objects.filter(ocupada=False)
+
+   
+
 class CitaEditarForm(forms.ModelForm):
     class Meta:
         model = Cita
-        fields = ['paciente', 'clinica', 'doctor', 'fecha', 'hora_inicio', 'hora_termino', 'estado_cita'] 
+        fields = ['paciente', 'clinica', 'dentista', 'fecha', 'hora_trabajo','estado_cita'] 
         labels = {
             'paciente' : "Paciente",
-            'doctor': "Doctor",
+            'dentista': "Dentista",
             'fecha' : "Fecha",  
             'clinica' : 'Clinicas',         
-            'hora_inicio' : "Hora de inicio",
-            'hora_termino' : "Hora de termino",
+            'hora_trabajo' : "Horario Seleccionado",
             'estado_cita' : "Estado de la cita"
+
 
         }
         widgets = {
             'paciente': forms.Select(attrs={'class': 'form-select'}),
-            'doctor': forms.Select(attrs={'class': 'form-select'}),
+            'dentista': forms.Select(attrs={'class': 'form-select'}),
             'clinica' : forms.Select(attrs={'class': 'form-select'}), 
-            'fecha': forms.DateInput(attrs={'class': 'form-control', 'type':'date'}),
-            'hora_inicio' : forms.TimeInput(attrs={'class': 'form-control', 'type':'time'}),            
-            'hora_termino' : forms.TimeInput(attrs={'class': 'form-control', 'type':'time'}),
+             'fecha': forms.DateInput(attrs={'class': 'form-control', 'type':'date'}),
+            'hora_trabajo' : forms.Select(attrs={'class': 'form-control'}),
             'estado_cita': forms.RadioSelect(attrs={'class': 'form-check', 'type':'radio'})
             
         }
@@ -79,5 +83,5 @@ class CitaEditarForm(forms.ModelForm):
         self.fields['paciente'].queryset = CustomUser.objects.filter(groups=pacientes_group)
 
         # Filtrar opciones del campo doctor según el grupo "doctores"
-        doctores_group = Group.objects.get(name='Doctores')
-        self.fields['doctor'].queryset = CustomUser.objects.filter(groups=doctores_group)
+        dentista_group = Group.objects.get(name='Dentista')
+        self.fields['dentista'].queryset = CustomUser.objects.filter(groups=dentista_group)
