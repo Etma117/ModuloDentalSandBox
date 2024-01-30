1828135c8383eef27a19ef9a4f1fe5b616161cd5
GER2430 <gary.inter3.e01@gmail.com>
2024-01-30 14:57:13 -0600

    contadores

--- core/generales/views.py
+++ core/generales/views.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from clinicas.models import Clinica
+from usuarios.models import CustomUser
 
 #Importamos los decoradores para crear que cada vista tenga su login required 
 
@@ -19,6 +21,9 @@ class home(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['total_clinicas'] = Clinica.objects.count()
+        context['total_dentistas'] = CustomUser.objects.filter(tipo_usuario="dentista").count()
+        context['total_responsables'] = CustomUser.objects.filter(tipo_usuario="responsable").count()
         context['navbar'] = 'home'  
         return context
 
