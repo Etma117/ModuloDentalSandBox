9ff1c42132bb3a221cd428f048c05009c4402a69
Dany248118 <ouen_daniel@hotmail.com>
2024-01-31 14:42:25 -0600

    no se

--- core/clinicas/forms.py
+++ core/clinicas/forms.py
@@ -5,4 +5,6 @@ class clinicaForm(forms.ModelForm):
     class Meta:
         model = Clinica
         fields = '__all__'
- 
+        widgets = {
+            'responsables': forms.Select(attrs={'class':'form-control'})
+        }
