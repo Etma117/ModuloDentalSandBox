51d93d8b760990c3c9ecebb44d1dc6d519f4aa0c
Ivan <ivansz123@gmail.com>
2024-01-31 14:02:30 -0600

    nuevos cambios con respecto y seguridad

--- core/generales/urls.py
+++ core/generales/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import home, exit, recover_password, set_new_password, verify_security_answers
+from .views import home, exit, recover_password, set_new_password, templeteDenegado, verify_security_answers
 
 
 from django.conf.urls.static import static
@@ -11,7 +11,7 @@ urlpatterns = [
     path('recover-password/', recover_password, name='recover_password'),
     path('verify-security-answers/', verify_security_answers, name='verify_security_answers'),
     path('set-new-password/', set_new_password, name='set_new_password'),
-
+    path('denegado/', templeteDenegado.as_view(), name="denegado")
    
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
