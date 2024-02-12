ea4219384e1ba8387230b8bfcdae25ce9ff2d478
ivansoza <ivansz123@gmail.com>
2024-02-12 12:43:57 -0600

    nuevos ajustes

--- core/generales/urls.py
+++ core/generales/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import home, exit, homePageView, recover_password, set_new_password, templeteDenegado, verify_security_answers
+from .views import home, exit, homePageView, login_redirect, recover_password, set_new_password, templeteDenegado, verify_security_answers
 
 
 from django.conf.urls.static import static
@@ -14,7 +14,8 @@ urlpatterns = [
     path('recover-password/', recover_password, name='recover_password'),
     path('verify-security-answers/', verify_security_answers, name='verify_security_answers'),
     path('set-new-password/', set_new_password, name='set_new_password'),
-    path('denegado/', templeteDenegado.as_view(), name="denegado")
-   
+    path('denegado/', templeteDenegado.as_view(), name="denegado"),
+    path('login/redirect/', login_redirect, name='login_redirect'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
