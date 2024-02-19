e5a0273002c05d4102548756a47d3269f73a7334
GER2430 <gary.inter3.e01@gmail.com>
2024-02-19 09:36:18 -0600

    no se

--- core/clinicas/urls.py
+++ core/clinicas/urls.py
@@ -3,13 +3,8 @@ from . import views
 from .views import clinicaCrear, Clinicas, vistaClinica, eliminarClinica, editarClinica
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('verClinicas/', Clinicas.as_view(), name='clinicas'),
-    path('clinica/<int:clinica_id>', vistaClinica.as_view(), name='vistaClinicas'),
-=======
     path('ver-clinicas/', Clinicas.as_view(), name='clinicas'),
     path('clinica/<int:pk>', vistaClinica.as_view(), name='vistaClinicas'),
->>>>>>> origin/master
     path('crearClinica/', clinicaCrear.as_view(), name='nuevaClinica'),
     path('eliminarClinica/<int:pk>', eliminarClinica.as_view(), name='eliminarClinica'),
     path('editarClinica/<int:clinica_id>', editarClinica.as_view(), name='editarClinica'),
