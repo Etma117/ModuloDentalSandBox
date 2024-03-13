8b29c25df10f8123f516aff8921604ce37582295
Etma117 <polllfuentes@gmail.com>
2024-03-13 11:46:20 -0600

    No merge Aun - Pwa - Cachear datos del sitio

--- core/static/js/serviceworker.js
+++ core/static/js/serviceworker.js
@@ -1,13 +1,14 @@
-//ServiceWorker
-const CACHE_NAME = 'dental-cache-v1';
+const CACHE_NAME = 'dental-cache-' + new Date().getTime();
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
+        '/offline/',
         '/',
         '/static/css/style.css',
         '/static/js/main.js',
+        // Agrega aquí más recursos estáticos que desees cachear
       ]).catch((error) => {
         console.error('Error al agregar recursos al caché:', error);
       });
@@ -15,13 +16,12 @@ self.addEventListener('install', (event) => {
   );
 });
 
-
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).then((fetchResponse) => {
         // Cachear nuevos recursos durante la solicitud
-        if(fetchResponse && fetchResponse.status === 200) {
+        if (fetchResponse && fetchResponse.status === 200) {
           const responseToCache = fetchResponse.clone();
           caches.open(CACHE_NAME).then((cache) => {
             cache.put(event.request, responseToCache);
@@ -29,6 +29,11 @@ self.addEventListener('fetch', (event) => {
         }
         return fetchResponse;
       });
+    }).catch(() => {
+      // Si falla la solicitud y no hay caché disponible, devuelve una página de fallback offline
+      if (event.request.mode === 'navigate') {
+        return caches.match('/offline/');
+      }
     })
   );
 });
@@ -44,5 +49,19 @@ self.addEventListener('activate', (event) => {
         })
       );
     })
+  );
+});
+
+// Escucha el evento de conexión a Internet
+self.addEventListener('online', (event) => {
+  // Cuando se detecta una conexión, actualiza el caché con los datos más recientes
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return fetch('/actualizar-datos/')  // Ruta a la vista que actualiza los datos
+          .then((response) => {
+            return cache.put('/datos/', response); // Almacena la respuesta actualizada en el caché
+          });
+      })
   );
 });
